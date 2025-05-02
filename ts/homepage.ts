259b2b8d0489c9dd58d369272f423c18ab9878cf
boithanh <boithanh01694@gmail.com>
2025-05-02 23:04:02 +0700

    add some logic html

--- ts/homepage.ts
+++ ts/homepage.ts
@@ -1,6 +1,6 @@
 document.querySelector("body")!.innerHTML = `
 <!-- Breadcrumb -->
-<Header>
+<header>
 <nav class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700" aria-label="Breadcrumb">
   <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
     <li class="inline-flex items-center">
@@ -8,11 +8,11 @@ document.querySelector("body")!.innerHTML = `
       </div>
     </li>
     <li>
-      <div class="flex items-center">
-        <svg class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+      <div class="flex items-center text-white">
+        <svg class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
         </svg>
-        <a href="./any-random.html" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Templates</a>
+        <a href="./any-random.html" class="ms-1 text-sm font-medium text-white hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Templates</a>
       </div>
     </li>
     <li aria-current="page">
@@ -20,12 +20,12 @@ document.querySelector("body")!.innerHTML = `
         <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
         </svg>
-        <a href="./find-and-choose-number.html" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Find & choose number</a>
+        <a href="./find-and-choose-number.html" class="ms-1 text-sm font-medium text-white hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Find & choose number</a>
         
     </li>
   </ol>
 </nav>
-</Header>
+</header>
 <main>
         <section id="numberPanel">
             <div class="container">
