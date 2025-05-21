b270b680e71b7981711d6b4b4ad27e7fba5101e7
boithanh <boithanh01694@gmail.com>
2025-05-21 12:11:31 +0700

    Restructure folder build

---
+++ public/public.md
@@ -0,0 +1,1 @@
+//this file contain some word for buiding success
--- scripts/build.js
+++ scripts/build.js
@@ -1,25 +1,31 @@
 // scripts/build.js
 const path = require('path');
 const fs = require('fs-extra');
-const { execSync } = require('child_process');
+// const { execSync } = require('child_process');
 
 // Folder copy
-const arrSrcFolder = ['../obfuscated/js', '../image', '../view', '../css'];
-const destFolderName = 'public';
+const arrSrcFolder = ['../image', '../view', '../css', '../icons'];
+const destFolderName = 'dist';
 for (let folder of arrSrcFolder) {
     const srcSourceInput = path.resolve(__dirname, folder);
-    const srcSourceOutput = path.resolve(__dirname, `../${destFolderName}/${folder.replace(/\.\.\/obfuscated\/?|\.\.\//g, "")}`);
+    const srcSourceOutput = path.resolve(__dirname, `../${destFolderName}/${folder.replace(/\.\.\//g, "")}`);
     console.log(`📂 Copying ${folder}`);
     fs.copySync(srcSourceInput, srcSourceOutput, {
         overwrite: true,
         errorOnExist: false,
         recursive: true,
     });
-    console.log(`✅ Build ${destFolderName}/${folder.replace(/\.\.\/obfuscated\/?|\.\.\//g, "")} hoàn tất!`);
+    console.log(`✅ Build ${destFolderName}/${folder.replace(/\.\.\//g, "")} hoàn tất!`);
 }
 
-//File copy
+//index.html copy
 fs.copySync(path.resolve(__dirname, '../index.html'), path.resolve(__dirname, `../${destFolderName}/index.html`), {
+    overwrite: true,
+    errorOnExist: false,
+    recursive: true,
+});
+//manifest.json copy
+fs.copySync(path.resolve(__dirname, '../manifest.json'), path.resolve(__dirname, `../${destFolderName}/manifest.json`), {
     overwrite: true,
     errorOnExist: false,
     recursive: true,
